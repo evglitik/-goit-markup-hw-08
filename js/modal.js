63756630672993d0905abcc31c0135f60f6ec3ea
Yevhen Lytvynchuk <evg.litik@gmail.com>
2022-08-07 11:31:36 +0300

    fix modal

--- js/modal.js
+++ js/modal.js
@@ -5,10 +5,22 @@
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', modalOpen);
+  refs.closeModalBtn.addEventListener('click', modalCloseBtn);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  function modalOpen() {
+    refs.modal.classList.remove('is-hidden');
+    refs.modal.addEventListener('click', modalCloseBg)
+  }
+
+  function modalCloseBtn() {
+    refs.modal.classList.add('is-hidden');
+  }
+
+  function modalCloseBg(e) {
+    if (e.target === refs.modal) {
+      refs.modal.removeEventListener('click', modalCloseBg);
+      modalCloseBtn();
+    }
   }
 })();
